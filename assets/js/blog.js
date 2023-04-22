157e2ad0432e2dbb7120385cf2778e57c4d60225
Dejatori <deivitsc@gmail.com>
2023-04-21 19:56:42 -0500

    Cositas al blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -19,6 +19,7 @@
 // }
 
 var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal2");
 var btn = document.querySelector(".post .btn");
 var span = document.getElementsByClassName("close")[0];
 
